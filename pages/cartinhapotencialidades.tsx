6e4c80212429ccab8e1d22f748ef53c192a9af69
System Administrator <root@Rodrigos-MacBook-Air.local>
2022-05-26 08:06:09 -0300

    cartinha build

--- pages/cartinhapotencialidades.tsx
+++ pages/cartinhapotencialidades.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { Document, Page, pdfjs } from "react-pdf";
+import React, { useState } from 'react';
+import { Document, Page, pdfjs } from 'react-pdf';
 import { FaWhatsapp } from 'react-icons/fa';
 import type { GetStaticProps, NextPage } from 'next';
 import { useRouter } from 'next/router';
@@ -33,14 +33,12 @@ export const getStaticProps: GetStaticProps = async () => ({
 });
 
 const Cartinha = (props: any) => {
-    
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber] = useState(1);
 
-    const [numPages, setNumPages] = useState(null);
-    const [pageNumber] = useState(1);
-
-    const onDocumentLoadSuccess = ({ }) => {
-        setNumPages(numPages);
-      };
+  const onDocumentLoadSuccess = ({ }) => {
+    setNumPages(numPages);
+  };
     
 
   return (
@@ -48,15 +46,15 @@ const Cartinha = (props: any) => {
     <Stack bgColor={'purple.200'}  align={'center'} justify={'center'} >
       <VStack  w={'80%'} direction={{ base: 'column', md: 'row' }}>
         <Heading  as='h3' size='lg' color={'purple.700'} paddingBottom={2}>
-            Cartinha Potencialidades
+          Cartinha Potencialidades
         </Heading>
         <Box   maxW={'99%'} borderRadius={2} boxShadow='lg'  rounded='xl' bg='white'>
-        <Document
-        file="/cartinha.pdf"
-        onLoadSuccess={onDocumentLoadSuccess}
-      >
-        <Page pageNumber={pageNumber} />
-      </Document>
+          <Document
+            file="/cartinha.pdf"
+            onLoadSuccess={onDocumentLoadSuccess}
+          >
+            <Page pageNumber={pageNumber} />
+          </Document>
         </Box> 
       </VStack>
     </Stack>
